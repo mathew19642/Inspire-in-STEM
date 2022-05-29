04005970e4f07ae0e5572b0ea4abaa72417d04b0
mathew19642 <mathewgitonga761@gmail.com>
2022-05-30 00:22:41 +0300

    assignment on geometric expression

--- assignment7.py
+++ assignment7.py
@@ -1,4 +1,6 @@
 #calculate the geometric progression
+#calculate the sum of the geometric expression
+#calculate the t_n ter / no of term
 ###############################
 
 
@@ -6,7 +8,7 @@ a =int(input("enter the first number"))
 r =int(input("enter the common ratio"))
 n =int(input("enter the number of terms"))
 
-total = (a * (1 - pow(r,n)))
-tn = a * pow(r,n-1)
+total = (a * (1 - pow(r,n)))/(1-r)   #sum of the GP
+tn = a * pow(r,n-1)    #t_n term of the GP
 print(total)
 print(tn)
